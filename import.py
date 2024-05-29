4ee1fd1afdf4155a22d3f99e6806c1a3cbb0bf86
PRODIGY_ML_01 <susrithposipogu@gmail.com>
2024-05-29 19:22:08 +0530

    add

---
+++ import.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+#house_price_dataset = sklearn.datasets.load_boston()
+#print(house_price_dataset)
